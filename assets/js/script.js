3b0f5fd48e085090b76e01f46ead397f27eedefb
Cris-San <cris629.cso@gmail.com>
2024-11-02 20:36:43 -0500

    Se crea la funcion dibujarCards() que transformara los datos y los mostrará con html dinamico

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,26 @@
    //Extrae la info de la api, la transforma en un objeto y espera para mostrarla.
    const data = await res.json();
 
+   dibujaCards(data);
+    
    console.log(data);
+  }
+
+  //Funcion para recibir,dibujar los paises y transformar en html visible
+  function dibujaCards(paises) {
+    const galeria = document.querySelector('.gallery');
+    let htmlPaises = '';// Declaramos la variable donde se guardará el HTML concatenado
+    paises.forEach((pais) => {
+      htmlPaises += `
+      <div class="card">
+        <h2>${pais.name.official}</h2>
+        <p><strong>Capital:</strong> ${pais.capital ? pais.capital[0] :'No Disponible'}</p>
+        <p><strong>Idioma:</strong> ${Object.values(pais.languages || {'': 'No Disponible' }).join(', ')}</p>
+        <button onclick="viewMore('${pais.cca3}')">Ver más</button>
+      </div>
+      `;
+    });
+
+    galeria.innerHTML = htmlPaises;
   }
   getDatos();
